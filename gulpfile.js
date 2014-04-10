d8afa7cfac1de372d39084736389c479e639e157
Johann Pardanaud <pardanaud.j@gmail.com>
2014-04-10 20:41:48 +0200

    Add a "watch" task to the gulpfile, allowing to compile the project whenever a file is changed.
    Also added a task to generate a minified file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,66 @@
-var gulp = require('gulp'),
-    rjs = require('gulp-requirejs'),
-    uglify = require('gulp-uglify');
+var fs = require('fs'),
+    gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    requirejs = require('requirejs'),
+    Q = require('q');
 
-gulp.task('optimize', function() {
-    return rjs({
+// Prepares the files used by the RequireJS optimizer.
+gulp.task('rjs-prepare', function() {
+    return gulp.src('node_modules/almond/almond.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/.tmp/'));
+});
+
+// Executes the RequireJS optimizer. Uses the original project and not a Gulp plugin since no one of them seems to
+// support to be run from a watching event.
+gulp.task('rjs-process', ['rjs-prepare'], function() {
+    var deferred = Q.defer();
+
+    requirejs.optimize({
         baseUrl: 'client',
+
         paths: {
-            'almond': '../node_modules/almond/almond'
+            'almond': '../dist/.tmp/almond'
         },
+
         include: [
             'almond',
             'speedtest'
         ],
-        out: 'speedtest.js',
-        wrap: true
-    })
-    .pipe(gulp.dest('dist/'));
+
+        optimize: 'none',
+        wrap: true,
+
+        out: function(content) {
+            fs.writeFile('dist/speedtest.js', content, deferred.resolve);
+        }
+    });
+
+    return deferred.promise;
 });
 
-gulp.task('default', ['optimize']);
+// Creates a minified version of SpeedTest.
+gulp.task('minify', ['rjs-process'], function() {
+    return gulp.src('dist/speedtest.js')
+        .pipe(uglify())
+        .pipe(rename('speedtest.min.js'))
+        .pipe(gulp.dest('dist/'));
+});
+
+// Cleans the temporary files.
+gulp.task('clean', ['rjs-process'], function() {
+    gulp.src('dist/.tmp', {read: false})
+        .pipe(clean());
+});
+
+// Watches for any file changes and runs the default task if needed.
+gulp.task('watch', function() {
+    var watcher = gulp.watch('client/**/*', ['default']);
+    watcher.on('change', function(event) {
+        console.log('File '+ event.path +' was '+ event.type +', running tasks...');
+    });
+});
+
+gulp.task('default', ['rjs-prepare', 'rjs-process', 'minify', 'clean']);
