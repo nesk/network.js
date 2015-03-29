nesk/network.js

commit 8d3c6580df4031f02dbc062dc76c0a3e69e0230e

Arrow functions, arrow functions everywhere!

client/modules/http.js
@@ -33,22 +33,20 @@ export default class HttpModule extends EventDispatcher {
 
     _initHttpConfig()
     {
-        var _this = this;
-
         // Each time a request starts or ends, set the requesting value unless it has been overridden with the
         // _setRequesting() method.
-        var loadstart = function() {
-            if (!_this._requestingOverridden) {
-                _this._requesting = true;
+        var loadstart = () => {
+            if (!this._requestingOverridden) {
+                this._requesting = true;
             }
         };
 
         this.on('xhr-loadstart', loadstart);
         this.on('xhr-upload-loadstart', loadstart);
 
-        var loadend = function() {
-            if (!_this._requestingOverridden) {
-                _this._requesting = false;
+        var loadend = () => {
+            if (!this._requestingOverridden) {
+                this._requesting = false;
             }
         };
 
@@ -66,8 +64,7 @@ export default class HttpModule extends EventDispatcher {
             return this;
         }
 
-        var _this = this,
-            options = this._options,
+        var options = this._options,
             xhr = new XMLHttpRequest(),
             validHttpMethods = ['GET', 'POST'];
 
@@ -87,7 +84,7 @@ export default class HttpModule extends EventDispatcher {
         var url = options.endpoint;
             url += (~url.indexOf('?') ? '&' : '?') + 'module=' + this._moduleName;
 
-        Object.keys(queryParams).forEach(function(param) {
+        Object.keys(queryParams).forEach(param => {
             url += '&' + param + '=' + encodeURIComponent(queryParams[param]);
         });
 
@@ -109,20 +106,20 @@ export default class HttpModule extends EventDispatcher {
         // Bind all the XHR events.
         var eventTypes = ['loadstart', 'progress', 'abort', 'error', 'load', 'timeout', 'loadend', 'readystatechange'];
 
-        eventTypes.forEach(function(eventType) {
-            xhr.addEventListener(eventType, function() {
+        eventTypes.forEach(eventType => {
+            xhr.addEventListener(eventType, () => {
                 // A last progress event can be triggered once a request has timed out, ignore it.
-                if (eventType == 'progress' && !_this._requesting) {
+                if (eventType == 'progress' && !this._requesting) {
                     return;
                 }
 
-                _this.trigger('xhr-'+ eventType, xhr, ...arguments);
+                this.trigger('xhr-'+ eventType, xhr, ...arguments);
             });
 
             // The XMLHttpRequestUpload interface supports all the above event types except the "readystatechange" one
             if (eventType != 'readystatechange') {
-                xhr.upload.addEventListener(eventType, function() {
-                    _this.trigger('xhr-upload-'+ eventType, xhr, ...arguments);
+                xhr.upload.addEventListener(eventType, () => {
+                    this.trigger('xhr-upload-'+ eventType, xhr, ...arguments);
                 });
             }
         });
@@ -154,17 +151,17 @@ export default class HttpModule extends EventDispatcher {
     {
         // The Resource Timing entries aren't immediately available once the 'load' event is triggered by an
         // XMLHttpRequest, we must wait for another process tick to check for a refreshed list.
-        setTimeout((function(lastURLToken) {
-            return function() {
+        setTimeout(() => {
+            return () => {
                 // Filter the timing entries to return only the one concerned by the last request made
-                var entries = performance.getEntriesByType('resource').filter(function(entry) {
-                    return ~entry.name.indexOf(lastURLToken);
+                var entries = performance.getEntriesByType('resource').filter(entry => {
+                    return ~entry.name.indexOf(this._lastURLToken);
                 });
 
                 // Return the entry through the callback
                 typeof callback == 'function' && callback(entries.length ? entries[0] : null);
             };
-        })(this._lastURLToken), 0);
+        }, 0);
 
         return this;
     }

client/event-dispatcher.js
@@ -50,7 +50,7 @@ export default class EventDispatcher {
         // cancelling an action.
         var returnValue = true;
 
-        events.forEach(function(callback) {
+        events.forEach(callback => {
             // A callback must explicitly return false if it wants the trigger() method to return false, undefined will
             // not work. This avoids crappy callbacks to mess up with the triggering system.
             var value = callback(...extraParameters);

client/modules/bandwidth.js
@@ -89,25 +89,13 @@ export default class Bandwidth extends HttpModule {
 
     _initBandwidthConfig()
     {
-        var _this = this,
-            loadingType = this._loadingType,
+        var loadingType = this._loadingType,
             eventsPrefix = (loadingType == 'upload') ? 'xhr-upload-' : 'xhr-';
 
-        this.on(eventsPrefix +'loadstart', function() {
-            Timing.mark(_this._timingLabels.start);
-        });
-
-        this.on(eventsPrefix +'progress', function(event) {
-            _this._progress(event);
-        });
-
-        this.on(eventsPrefix +'timeout', function() {
-            _this._timeout();
-        });
-
-        this.on(eventsPrefix +'loadend', function() {
-            _this._end();
-        });
+        this.on(eventsPrefix +'loadstart', () => Timing.mark(this._timingLabels.start));
+        this.on(eventsPrefix +'progress', event => this._progress(event));
+        this.on(eventsPrefix +'timeout', () => this._timeout());
+        this.on(eventsPrefix +'loadend', () => this._end());
     }
 
     _progress(event)

client/modules/latency.js
@@ -49,19 +49,17 @@ export default class LatencyModule extends HttpModule {
 
     _initLatencyConfig()
     {
-        var _this = this;
-
         // Calculate the latency with the Resource Timing API once the request is finished.
         if (Timing.supportsResourceTiming()) {
-            this.on('xhr-load', function() {
-                _this._getTimingEntry(function(entry) {
+            this.on('xhr-load', () => {
+                this._getTimingEntry(entry => {
                     // The latency calculation differs between an HTTP and an HTTPS connection.
                     // See: http://www.w3.org/TR/resource-timing/#processing-model
                     var latency = !entry.secureConnectionStart
                                     ? entry.connectEnd - entry.connectStart
                                     : entry.secureConnectionStart - entry.connectStart;
 
-                    _this._latencies.push(latency);
+                    this._latencies.push(latency);
                 });
             });
         }
@@ -71,24 +69,20 @@ export default class LatencyModule extends HttpModule {
             var labels = this._timingLabels;
 
             // Set a mark when the request starts.
-            this.on('xhr-loadstart', function() {
-                Timing.mark(labels.start);
-            });
+            this.on('xhr-loadstart', () => Timing.mark(labels.start));
 
-            this.on('xhr-readystatechange', function(xhr) {
+            this.on('xhr-readystatechange', xhr => {
                 // Ignore the first request (see the comments in the start() method) and calculate the latency if the
                 // headers have been received.
-                if (_this._requestsLeft < 5 && xhr.readyState == XMLHttpRequest.HEADERS_RECEIVED) {
+                if (this._requestsLeft < 5 && xhr.readyState == XMLHttpRequest.HEADERS_RECEIVED) {
                     // Save the timing measure.
                     Timing.mark(labels.end);
-                    _this._latencies.push(Timing.measure(labels.measure, labels.start, labels.end));
+                    this._latencies.push(Timing.measure(labels.measure, labels.start, labels.end));
                 }
             });
         }
 
-        this.on('xhr-load', function() {
-            _this._nextRequest();
-        });
+        this.on('xhr-load', () => this._nextRequest());
     }
 
     _nextRequest()
@@ -105,17 +99,13 @@ export default class LatencyModule extends HttpModule {
             // Create the new request and send it.
             this._newRequest('GET')._sendRequest();
         } else {
-            var _this = this;
-
             // All the requests are finished, set the requesting status to false.
             this._setRequesting(false);
 
             // If all the requests have been executed, calculate the average latency. Since the _getTimingEntry() method
             // is asynchronous, wait for the next process tick to execute the _calculate() method, to be sure that all
             // the latencies have been retrieved.
-            setTimeout(function() {
-                _this._calculate();
-            }, 0);
+            setTimeout(() => this._calculate(), 0);
         }
     }
 
@@ -125,7 +115,7 @@ export default class LatencyModule extends HttpModule {
             isThereAnyZeroLatency = false;
 
         // Get the average latency.
-        var avgLatency = latencies.reduce(function(a, b) {
+        var avgLatency = latencies.reduce((a, b) => {
             // Check if there is any latency equal to zero.
             isThereAnyZeroLatency = isThereAnyZeroLatency || (a == 0 || b == 0);
             // Sum the current latency to the previous value.

commit 8706b55d39fa6010b750444252bdbe2cc6b1b4d6

Fix a bug where latency calculation fails
Bug introduced by commit 8d3c658

client/modules/http.js
@@ -145,17 +145,17 @@ export default class HttpModule extends EventDispatcher {
     {
         // The Resource Timing entries aren't immediately available once the 'load' event is triggered by an
         // XMLHttpRequest, we must wait for another process tick to check for a refreshed list.
-        setTimeout(() => {
+        setTimeout((lastURLToken => {
             return () => {
                 // Filter the timing entries to return only the one concerned by the last request made
-                var entries = performance.getEntriesByType('resource').filter(entry => {
-                    return ~entry.name.indexOf(this._lastURLToken);
+                var entries = performance.getEntriesByType('resource').filter(function(entry) {
+                    return ~entry.name.indexOf(lastURLToken);
                 });
 
                 // Return the entry through the callback
                 typeof callback == 'function' && callback(entries.length ? entries[0] : null);
             };
-        }, 0);
+        })(this._lastURLToken), 0);
 
         return this;
     }