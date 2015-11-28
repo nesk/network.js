6b1c7974503ced7fe96d6c700a5a49759c096b18
Johann Pardanaud <pardanaud.j@gmail.com>
2015-11-28 18:05:02 +0100

    Improve code style

--- utils/helpers.js
+++ utils/helpers.js
@@ -26,8 +26,7 @@ export function getGlobalObject() {
  * @param {*} obj The value to check.
  * @returns {boolean} `true` if the value is an object, otherwise `false`.
  */
-export function isObject(obj)
-{
+export function isObject(obj) {
     return obj != undefined && obj != null && typeof obj.valueOf() == 'object';
 }
 
@@ -38,13 +37,12 @@ export function isObject(obj)
  * @param {*} value The value to copy.
  * @returns {*} The copied value.
  */
-export function copy(value)
-{
+export function copy(value) {
     return JSON.parse(JSON.stringify(value));
 }
 
 /**
- * Copy the properties in the source objects over to the destination object.
+ * Deeply copy the properties in the source objects over to the destination object.
  * @private
  * @function _assign
  * @param {boolean} strict Given `true`, new properties will not be copied.
@@ -52,8 +50,7 @@ export function copy(value)
  * @param {...Object} sources The source objects.
  * @returns {Object} The destination object once the properties are copied.
  */
-function _assign(strict, target = {}, ...sources)
-{
+function _assign(strict, target = {}, ...sources) {
     target = copy(target);
 
     sources.forEach(source => {
@@ -69,28 +66,26 @@ function _assign(strict, target = {}, ...sources)
 }
 
 /**
- * Copy all the properties in the source objects over to the destination object.
+ * Deeply copy all the properties in the source objects over to the destination object.
  * @private
  * @function assign
  * @param {Object} [target={}] The destination object.
  * @param {...Object} sources The source objects.
  * @returns {Object} The destination object once the properties are copied.
  */
-export function assign(target = {}, ...sources)
-{
+export function assign(target = {}, ...sources) {
     return _assign(false, target, ...sources);
 }
 
 /**
- * Copy the properties (but no new ones) in the source objects over to the destination object.
+ * Deeply copy the properties (but no new ones) in the source objects over to the destination object.
  * @private
  * @function assignStrict
  * @param {Object} [target={}] The destination object.
  * @param {...Object} sources The source objects.
  * @returns {Object} The destination object once the properties are copied.
  */
-export function assignStrict(target = {}, ...sources)
-{
+export function assignStrict(target = {}, ...sources) {
     return _assign(true, target, ...sources);
 }
 
@@ -102,8 +97,7 @@ export function assignStrict(target = {}, ...sources)
  * @param {string[]} properties The properties to exclude from the copied object.
  * @returns {Object} The copied object without the specified properties.
  */
-export function except(obj, properties)
-{
+export function except(obj, properties) {
     let objCopy = copy(obj);
 
     properties.forEach(index => delete objCopy[index]);
@@ -118,8 +112,7 @@ export function except(obj, properties)
  * @param {Function} func The function to defer.
  * @returns {Defer} The Defer object used to execute the function when needed.
  */
-export function defer(func = () => {})
-{
+export function defer(func = () => {}) {
     /**
      * @private
      * @class Defer
